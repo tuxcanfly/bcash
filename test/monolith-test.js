bb7540d3046a48b2504f8fe0629cab95e9c88e53
Javed Khan <tuxcanfly@gmail.com>
2018-05-29 21:28:18 +0530

    WIP: group op success test cases

--- test/monolith-test.js
+++ test/monolith-test.js
@@ -129,54 +129,55 @@ describe('Monolith', function() {
     assert.strictEqual(stack.toString(), expected.toString());
   });
 
-  const minimalTestCases = [
-    // [input, output]
-    // Test zero values
-    ['', ''],
-    ['00', ''],
-    ['80', ''],
-    ['0000', ''],
-    ['0080', ''],
-    // Non-zero values
-    ['01', '81'],
-    ['81', '01'],
-    ['0201', '0281'],
-    ['0281', '0201'],
-    ['ff0201', 'ff0281'],
-    ['ff0281', 'ff0201'],
-    ['ffff0201', 'ffff0281'],
-    ['ffff0281', 'ffff0201'],
-    // Should not be overly-minimized
-    ['ff80', 'ff00'],
-    ['ff00', 'ff80']
-  ];
-  for (const test of minimalTestCases) {
-    it(`should match minimal encoding ${test[0].toString('hex')}`, async () => {
+  it('should match minimal encoding', async () => {
+    const minimalTestCases = [
+      // [input, output]
+      // Test zero values
+      ['', ''],
+      ['00', ''],
+      ['80', ''],
+      ['0000', ''],
+      ['0080', ''],
+      // Non-zero values
+      ['01', '81'],
+      ['81', '01'],
+      ['0201', '0281'],
+      ['0281', '0201'],
+      ['ff0201', 'ff0281'],
+      ['ff0281', 'ff0201'],
+      ['ffff0201', 'ffff0281'],
+      ['ffff0281', 'ffff0201'],
+      // Should not be overly-minimized
+      ['ff80', 'ff00'],
+      ['ff00', 'ff80']
+    ];
+
+    for (const test of minimalTestCases) {
       const data = Buffer.from(test[0], 'hex');
       const expected = Buffer.from(test[1], 'hex');
       testMinimalNegative(data, expected);
-    });
-  }
+    }
+  });
 
-  const zeros = Buffer.alloc(520);
-  const ones = Buffer.alloc(520, 255);
-  const bitwiseTestCases = [
-    // [name, a, b, and, or, xor]
-    ['null x null', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-    // Run all variations of zeros and ones.
-    ['zeros x zeros', zeros, zeros, zeros, zeros, zeros],
-    ['zeros x ones', zeros, ones, zeros, ones, ones],
-    ['ones x ones', ones, ones, ones, ones, zeros]
-  ];
+  it('should match bitwise ops', async () => {
+    const zeros = Buffer.alloc(520);
+    const ones = Buffer.alloc(520, 255);
+    const bitwiseTestCases = [
+      // [name, a, b, and, or, xor]
+      ['null x null', EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+      // Run all variations of zeros and ones.
+      ['zeros x zeros', zeros, zeros, zeros, zeros, zeros],
+      ['zeros x ones', zeros, ones, zeros, ones, ones],
+      ['ones x ones', ones, ones, ones, ones, zeros]
+    ];
 
-  for (const test of bitwiseTestCases) {
-    it(`should match bitwise ops - ${test[0]}`, async () => {
+    for (const test of bitwiseTestCases) {
       const and = new Stack([test[3]]);
       const or = new Stack([test[4]]);
       const xor = new Stack([test[5]]);
       testBitwiseOps(test[1], test[2], and, or, xor);
-    });
-  }
+    }
+  });
 
   it('should fail bitwise for less than 2 stack items', async () => {
     let stack = new Stack();
@@ -216,24 +217,24 @@ describe('Monolith', function() {
     testBitwiseOpErrors(stack, 'INVALID_OPERAND_SIZE');
   });
 
-  const stringTestCases = [
-    // [a, b, expected]
-    // Check for empty string.
-    ['', '', ''],
-    // Check for simple concats.
-    ['00', '00', '0000'],
-    ['ab', 'cd', 'abcd'],
-    ['abcdef', '12345678', 'abcdef12345678']
-  ];
+  it('should match string ops', async () => {
+    const stringTestCases = [
+      // [a, b, expected]
+      // Check for empty string.
+      ['', '', ''],
+      // Check for simple concats.
+      ['00', '00', '0000'],
+      ['ab', 'cd', 'abcd'],
+      ['abcdef', '12345678', 'abcdef12345678']
+    ];
 
-  for (const test of stringTestCases) {
-    it(`should match string ops - ${test[0]} + ${test[1]}`, async () => {
+    for (const test of stringTestCases) {
       const a = Buffer.from(test[0], 'hex');
       const b = Buffer.from(test[1], 'hex');
       const stack = new Stack([test[2]]);
       testStringOp(a, b, stack);
-    });
-  }
+    }
+  });
 
   for (const op of ['OP_CAT', 'OP_SPLIT']) {
     it(`should fail ${op} for less than 2 stack items`, async () => {
@@ -248,23 +249,23 @@ describe('Monolith', function() {
     });
   }
 
-  const typeConversionTestCase = [
-    // [bin, num]
-    // Some known values.
-    ['abcdef00', 'abcdef00'],
-    ['abcd7f00', 'abcd7f'],
-    // Reductions
-    ['abcdef4280', 'abcdefc2'],
-    ['abcd7f4200', 'abcd7f42']
-  ];
+  it('should match type conversion ops', async () => {
+    const typeConversionTestCase = [
+      // [bin, num]
+      // Some known values.
+      ['abcdef00', 'abcdef00'],
+      ['abcd7f00', 'abcd7f'],
+      // Reductions
+      ['abcdef4280', 'abcdefc2'],
+      ['abcd7f4200', 'abcd7f42']
+    ];
 
-  for (const test of typeConversionTestCase) {
-    it(`should match type conversion ops - ${test[0]}.${test[1]}`, async () => {
+    for (const test of typeConversionTestCase) {
       const bin = Buffer.from(test[0], 'hex');
       const num = new Stack([Buffer.from(test[1], 'hex')]);
       testTypeConversionOp(bin, num);
-    });
-  }
+    }
+  });
 
   it('should fail type conversion for empty stack', async () => {
     const stack = new Stack();
@@ -307,45 +308,45 @@ describe('Monolith', function() {
     isError(stack, Script.fromString('OP_NUM2BIN'), 'IMPOSSIBLE_ENCODING');
   });
 
-  const arithmeticTestCases = [
-    // [a, b, div, mod]
-    // 0x185377af / 0x85f41b01 = -4
-    // 0x185377af % 0x85f41b01 = 0x00830bab
-    // 408123311 / -99883777 = -4
-    // 408123311 % -99883777 = 8588203
-    ['af775318', '011bf485', '84', 'ab0b8300'],
-    // 0x185377af / 0x00001b01 = 0xe69d
-    // 0x185377af % 0x00001b01 = 0x0212
-    // 408123311 / 6913 = 59037
-    // 408123311 % 6913 = 530
-    ['af775318', '011b', '9de600', '1202'],
-    // 15/4 = 3 (and negative operands)
-    ['0f', '04', '03', '03'],
-    // 15000/4 = 3750 (and negative operands)
-    ['983a', '04', 'a60e', ''],
-    // 15000/4000 = 3 (and negative operands)
-    ['983a', 'a00f', '03', 'b80b'],
-    // 15000000/4000 = 3750 (and negative operands)
-    ['c0e1e400', 'a00f', 'a60e', ''],
-    // 15000000/4 = 3750000 (and negative operands)
-    ['c0e1e400', '04', '703839', ''],
-    // 56488123 % 321 = 148 (and negative operands)
-    ['bbf05d03', '4101', '67af02', '9400'],
-    // 56488123 % 3 = 1 (and negative operands)
-    ['bbf05d03', '03', '3e501f01', '01'],
-    // 56488123 % 564881230 = 56488123 (and negative operands)
-    ['bbf05d03', '4e67ab21', '', 'bbf05d03']
-  ];
+  it('should match arithmetic ops', async () => {
+    const arithmeticTestCases = [
+      // [a, b, div, mod]
+      // 0x185377af / 0x85f41b01 = -4
+      // 0x185377af % 0x85f41b01 = 0x00830bab
+      // 408123311 / -99883777 = -4
+      // 408123311 % -99883777 = 8588203
+      ['af775318', '011bf485', '84', 'ab0b8300'],
+      // 0x185377af / 0x00001b01 = 0xe69d
+      // 0x185377af % 0x00001b01 = 0x0212
+      // 408123311 / 6913 = 59037
+      // 408123311 % 6913 = 530
+      ['af775318', '011b', '9de600', '1202'],
+      // 15/4 = 3 (and negative operands)
+      ['0f', '04', '03', '03'],
+      // 15000/4 = 3750 (and negative operands)
+      ['983a', '04', 'a60e', ''],
+      // 15000/4000 = 3 (and negative operands)
+      ['983a', 'a00f', '03', 'b80b'],
+      // 15000000/4000 = 3750 (and negative operands)
+      ['c0e1e400', 'a00f', 'a60e', ''],
+      // 15000000/4 = 3750000 (and negative operands)
+      ['c0e1e400', '04', '703839', ''],
+      // 56488123 % 321 = 148 (and negative operands)
+      ['bbf05d03', '4101', '67af02', '9400'],
+      // 56488123 % 3 = 1 (and negative operands)
+      ['bbf05d03', '03', '3e501f01', '01'],
+      // 56488123 % 564881230 = 56488123 (and negative operands)
+      ['bbf05d03', '4e67ab21', '', 'bbf05d03']
+    ];
 
-  for (const test of arithmeticTestCases) {
-    it(`should match arithmetic ops - ${test[0]} x ${test[1]}`, async () => {
+    for (const test of arithmeticTestCases) {
       const a = Buffer.from(test[0], 'hex');
       const b = Buffer.from(test[1], 'hex');
       const div = new Stack([Buffer.from(test[2], 'hex')]);
       const mod = new Stack([Buffer.from(test[3], 'hex')]);
       testDivModOp(a, b, div, mod);
-    });
-  }
+    }
+  });
 
   for (const op of ['OP_DIV', 'OP_MOD']) {
     it(`should fail ${op} for less than 2 stack items`, async () => {
